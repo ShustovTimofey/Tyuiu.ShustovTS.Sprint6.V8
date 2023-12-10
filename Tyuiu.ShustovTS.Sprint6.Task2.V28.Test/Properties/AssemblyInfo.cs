120a317cd755355a97a63d494ef6547d90ee231d
Шустов Тима <tima_shustov005@mail.ru>
2023-12-10 21:54:52 +0500

    f

---
+++ Tyuiu.ShustovTS.Sprint6.Task2.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShustovTS.Sprint6.Task2.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShustovTS.Sprint6.Task2.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("171d5400-ceaf-42bc-8906-b685b813c6e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShustovTS.Sprint6.Task3.V26/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.ShustovTS.Sprint6.Task3.V26.Lib;
+
+namespace Tyuiu.ShustovTS.Sprint6.Task3.V26
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+        int[,] mtrx = new int[5, 5] { { 16, 19, 17, 2, 8 },
+                                       { -17, 8, -17, -8, 1 },
+                                       { -7, 17, -2, 1, -3 },
+                                       { -12, 0, -17, 15, 6 },
+                                       { 17, -6, -17, 18, -19 } };
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            dataGridViewMatrix_STS.ColumnCount = columns;
+            dataGridViewMatrix_STS.RowCount = rows;
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewMatrix_STS.Columns[i].Width = 30;
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewMatrix_STS.Rows[i].Cells[j].Value = Convert.ToString(mtrx[i, j]);
+                }
+
+
+            }
+
+        }
+
+        private void buttonReference_LVA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 3 выполнил студент группы АСОиУБ-23-2 Шустов Тимофей Сергеевич", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonResult_STS_Click(object sender, EventArgs e)
+        {
+
+            int[,] resultMatrix = ds.Calculate(mtrx);
+
+
+            dataGridViewMatrixResult_STS.Rows.Clear();
+            dataGridViewMatrixResult_STS.Columns.Clear();
+
+
+            int rows = resultMatrix.GetUpperBound(0) + 1;
+            int columns = resultMatrix.Length / rows;
+
+
+            dataGridViewMatrixResult_STS.ColumnCount = columns;
+            dataGridViewMatrixResult_STS.RowCount = rows;
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewMatrixResult_STS.Columns[i].Width = 30;
+            }
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewMatrixResult_STS.Rows[i].Cells[j].Value = resultMatrix[i, j].ToString();
+                }
+            }
+        }
+    }
+}
